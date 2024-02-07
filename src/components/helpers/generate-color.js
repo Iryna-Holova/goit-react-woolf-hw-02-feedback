f533333144faff750a380f084ae681b098235a1e
Iryna Holova <gis.golova@gmail.com>
2024-02-07 14:47:19 +0200

    fixed styles

--- src/components/helpers/generate-color.js
+++ src/components/helpers/generate-color.js
@@ -5,7 +5,7 @@ function generateColor(value) {
   let redHex = red.toString(16).padStart(2, '0');
   let greenHex = green.toString(16).padStart(2, '0');
 
-  return `#${redHex}${greenHex}50`;
+  return `#${redHex}${greenHex}30`;
 }
 
 export default generateColor;
